4294c2d5494258801803f199f2b09bcd18a02cd5
kamalakars04 <kamalakars04@gmail.com>
2020-10-10 09:15:18 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace AdditionSubtraction
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public int add(int numOne, int numTwo)
+        {
+            int sum = numOne + numTwo;
+            return sum;
+        }
+        public int addition(int numOne, int numTwo)
+        {
+            int sum = numOne + numTwo;
+            return sum;
+        }
+        public int substraction(int numOne, int numTwo)
+        {
+            int sum = numOne - numTwo;
+            return sum;
+        }
+        public int sub(int numOne, int numTwo)
+        {
+            int sum = numOne - numTwo;
+            return sum;
+        }
+    }
+}
